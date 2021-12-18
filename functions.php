4910230e9c4468c29fb9043dfcd97eb583d60c46
D Marcus <mindlesscorporatedrone@gmail.com>
2021-12-17 16:08:44 -0800

    added thumbnail support

--- functions.php
+++ functions.php
@@ -25,6 +25,7 @@ function learnwp_config(){
 		'width' => 1920
 	);
 	add_theme_support( 'custom-header', $args );
+	add_theme_support( 'post-thumbnails' );
 
 }
 add_action( 'after_setup_theme', 'learnwp_config', 0 );
