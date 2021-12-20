548a3d3200d3e8a4f902334155a1ef8303df54fd
D Marcus <mindlesscorporatedrone@gmail.com>
2021-12-19 16:22:47 -0800

    added sidebars

--- functions.php
+++ functions.php
@@ -3,8 +3,8 @@
 // Including stylesheet and script files
 function load_scripts(){
 	wp_enqueue_script( 'bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js', array( 'jquery' ), '4.0.0', true );
-	wp_enqueue_style( 'bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css', array(), '4.0.0', 'all' );	
-	wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all' );
+	wp_enqueue_style( 'bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css', array(), '4.0.0', 'all' );
+	wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all' );	
 }
 add_action( 'wp_enqueue_scripts', 'load_scripts' );
 
@@ -30,3 +30,63 @@ function learnwp_config(){
 
 }
 add_action( 'after_setup_theme', 'learnwp_config', 0 );
+
+// Registering our sidebars
+add_action( 'widgets_init', 'learnwp_sidebars' );
+function learnwp_sidebars(){
+	register_sidebar(
+		array(
+			'name' => 'Home Page Sidebar',
+			'id' => 'sidebar-1',
+			'description' => 'This is the Home Page Sidebar. You can add your widgets here. ',
+			'before_widget' => '<div class="widget-wrapper">',
+			'after_widget' => '</div>',
+			'before_title' => '<h2 class="widget-title">',
+			'after_title' => '</h2>'
+		)
+	);
+	register_sidebar(
+		array(
+			'name' => 'Blog Sidebar',
+			'id' => 'sidebar-2',
+			'description' => 'This is the Blog Sidebar. You can add your widgets here. ',
+			'before_widget' => '<div class="widget-wrapper">',
+			'after_widget' => '</div>',
+			'before_title' => '<h2 class="widget-title">',
+			'after_title' => '</h2>'
+		)
+	);	
+	register_sidebar(
+		array(
+			'name' => 'Service 1',
+			'id' => 'services-1',
+			'description' => 'First Services Area. ',
+			'before_widget' => '<div class="widget-wrapper">',
+			'after_widget' => '</div>',
+			'before_title' => '<h2 class="widget-title">',
+			'after_title' => '</h2>'
+		)
+	);
+	register_sidebar(
+		array(
+			'name' => 'Service 2',
+			'id' => 'services-2',
+			'description' => 'Second Services Area. ',
+			'before_widget' => '<div class="widget-wrapper">',
+			'after_widget' => '</div>',
+			'before_title' => '<h2 class="widget-title">',
+			'after_title' => '</h2>'
+		)
+	);
+	register_sidebar(
+		array(
+			'name' => 'Service 3',
+			'id' => 'services-3',
+			'description' => 'Third Services Area. ',
+			'before_widget' => '<div class="widget-wrapper">',
+			'after_widget' => '</div>',
+			'before_title' => '<h2 class="widget-title">',
+			'after_title' => '</h2>'
+		)
+	);							
+}
